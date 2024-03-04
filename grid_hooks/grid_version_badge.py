cf123174b7908395b466d9654dcdab509c4dcbea
Bill <Bill@TheWallisFamily.co.uk>
2024-03-04 11:45:35 +0000

    ✨ feat: Add config files

---
+++ grid_hooks/grid_version_badge.py
@@ -0,0 +1,14 @@
+"""
+Pull the Grid template version number from the copier answers and save
+it as a badge.
+
+Inspired by:
+
+- https://github.com/dbrgn/coverage-badge
+"""
+
+
+def main() -> None:
+    """
+    Save the Grid template version number as a badge.
+    """
